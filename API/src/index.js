e61b862b875705235a0870114694e8e79e09b4f3
Samarth <samarth@pop-os.localdomain>
2020-08-31 17:34:33 +0530

    refactor

--- API/src/index.js
+++ API/src/index.js
@@ -1,8 +1,36 @@
 const express = require("express");
+const { exec } = require('child_process');
+const mkdirp = require('mkdirp')
+const fs = require('fs');
 const app = express();
 const port = 3000;
 
-app.get("*", (req, res) => {
+app.use(express.json());
+
+app.post("/compile", (req, res) => {
+
+  const data = req.body;
+  console.log(data);
+
+  mkdirp("./code/usercode").then((_) => 
+  {
+    fs.writeFile("./code/usercode/file.cpp", data.source_code, (err) => 
+    {
+    });
+  })
+
+  
+  //remove usercode folder
+
+  // setTimeout(() => 
+  // {
+  //   exec("rm -r ./code/usercode", (err, stdout, stderr) => 
+  //   {
+  //       console.log("deleting");
+  //   })
+  // }, 3000)
+
+
   res.send("index.js");
 });
 
