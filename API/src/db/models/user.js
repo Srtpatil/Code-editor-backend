157ef4c69640a6142d7826b9f588e69ef0528119
Kunal Ambekar <kunal04@pop-os.localdomain>
2020-09-03 21:52:45 +0530

    Authenication Added

--- API/src/db/models/user.js
+++ API/src/db/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -23,7 +24,42 @@ const userSchema = new mongoose.Schema({
     minlength: 8,
     trim: true,
   },
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
 });
+
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+  if (!user) {
+    return "User not found";
+  }
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    return "invalid Password";
+  }
+  return user;
+};
+
+userSchema.methods.generateAuthToken = async function () {
+  const user = this;
+  const token = jwt.sign(
+    {
+      _id: user._id.toString(),
+    },
+    "CodeEditorBE"
+  );
+  user.tokens = user.tokens.concat({
+    token: token,
+  });
+  await user.save();
+  return token;
+};
 
 userSchema.pre("save", async function (next) {
   const user = this;
